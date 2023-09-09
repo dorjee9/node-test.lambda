5dace3bcc6e1e9776b59f31d50d479272f177929
sonam <dorjee.tsering001@gmail.com>
2023-09-09 17:33:13 -0400

    addd

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 
 
 exports.hello = async(event) => {
-    const data =  event.body;
-    const buff = new Buffer.from(data, 'base64');
-    const reqString = buff.toString('ascii');
-    const reqJson = JSON.parse(reqString);
-
     const response = {
         statusCode : 200,
-        body: `Hello ${reqJson.name} from india`
+        body: `Hello from india`
     };
 
     return response;
